e1d9f8db39c9c6b76081ef143eb6bfd123e511d8
bgrrw <109300557+bgrrw@users.noreply.github.com>
2023-07-11 09:51:06 -0700

    Update GPX_Sources.js

--- GPX_Sources.js
+++ GPX_Sources.js
@@ -68,5 +68,8 @@ var listOfFilesPath = [
 
 var listOfPaddleboardGPX = [
   "GPX/11061360609.gpx",
-  "GPX/11069273960.gpx"
+  "GPX/11069273960.gpx",
+  "GPX/11417286906.gpx",
+  "GPX/11526231967.gpx",
+  "GPX/11526232508.gpx"
  ];

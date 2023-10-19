cf45bd51c20e0fcafdc53feeb8c1cfa384fcaffe
bgrrw <109300557+bgrrw@users.noreply.github.com>
2023-10-19 09:49:08 -0700

    Add files via upload

--- McIntyre.js
+++ McIntyre.js
@@ -4,7 +4,7 @@ var McIntyreSkiTracks = {
 		{
 			"type": "Feature", 
 			"properties": { 
-				"Name": "Mt. McIntyre Ski Trails 
+				"Name": "Mt. McIntyre Ski Trails" 
 			}, 
 			"geometry": { 
 				"type": "MultiLineString", 
